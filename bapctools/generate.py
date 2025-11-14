a012c50962e827af20824ad9180fee45ee6ce695
Niklas Mohrin <dev@niklasmohrin.de>
2025-11-14 15:12:08 +0100

    Run ruff isort

--- bapctools/generate.py
+++ bapctools/generate.py
@@ -11,12 +11,7 @@ from colorama import Fore, Style
 from pathlib import Path, PurePosixPath
 from typing import Any, cast, Final, Literal, Optional, overload
 
-from bapctools import config
-from bapctools import parallel
-from bapctools import program
-from bapctools import run
-from bapctools import validate
-from bapctools import visualize
+from bapctools import config, parallel, program, run, validate, visualize
 from bapctools.problem import Problem
 from bapctools.testcase import Testcase
 from bapctools.util import (
